468a210eb7a3845dc4585e5ec58c58783a7a1886
jvariara <jvariara@gmail.com>
2022-08-04 20:14:26 -0400

    Finished with Home Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import Discounted from "./components/Discounted";
+import Explore from "./components/Explore";
+import Featured from "./components/Featured";
+import Footer from "./components/Footer";
+import Highlights from "./components/Highlights";
+import Landing from "./components/Landing";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-      Test
+      <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
+      <Discounted />
+      <Explore />
+      <Footer />
     </div>
   );
 }
